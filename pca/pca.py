3ac6f0b535c812afd6970b307ba37b05a5aba1c3
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2021-08-17 16:30:47 -0400

    Finish PCA analysis

--- pca/pca.py
+++ pca/pca.py
@@ -11,6 +11,14 @@ from sklearn.model_selection import GridSearchCV
 
 from mpl_toolkits.mplot3d import Axes3D
 
+# Use supernatural enforcement variables as a proxy for kinship intensity
+COLS = [
+        'Supernatural_enforcement_of_fairness',
+        'Supernatural_enforcement_of_human_reciprocity',
+        'Supernatural_enforcement_of_ingroup_loyalty',
+        ]
+
+
 def truncate(x):
     if x < 0:
         return 0
@@ -46,78 +54,96 @@ def freqToScore(freq):
         'once in a lifetime'    : 1/60
     }[freq]/365
 
+# Check to see if a series meets the specified imputation threshold
+def imputationThreshold(series, maxImputed):
+    features_imputed = eval(series['Features_imputed'])
+    num_imputed = np.sum([features_imputed[c] for c in COLS])
+    return num_imputed <= maxImputed
 
+# Restrict data to the specified imputation threshold
+def restrict(df, maxImputed):
+    threshold = lambda series : imputationThreshold(series, maxImputed)
+    return df[COLS][df.apply(threshold, axis=1)].copy()
 
-def main():
-    # Read in data
-    data = pd.read_csv('shiny-seshat.csv',index_col=0)
-
-    # Specify columns
-    freqCols = [
-            'Largest_scale_ritual_frequency_per_participant',
-            'Most_dysphoric_ritual_frequency_per_participant',
-            'Most_euphoric_ritual_frequency_per_participant',
-            'Most_frequent_ritual_frequency_per_participant',
-    ]
- 
-    supernaturalCols = [
-            'Supernatural_enforcement_of_fairness',
-            'Supernatural_enforcement_of_human_reciprocity',
-            'Supernatural_enforcement_of_ingroup_loyalty',
-            ]
-
-    cols = freqCols + supernaturalCols
-
-    # Clean up desired columns
-    for col in freqCols:
-        data[col] = data[col].apply(freqToScore)
-
-    for col in cols:
-        data[col] = data[col].apply(truncate)
-
-    def imputationThreshold(series):
-        features_imputed = eval(series['Features_imputed'])
-        num_freq_imputed = np.sum([features_imputed[c] for c in freqCols])
-        num_supn_imputed = np.sum([features_imputed[c] for c in supernaturalCols])
-        return (num_freq_imputed <= 2) and (num_supn_imputed <= 1) 
-    # Restrict data to only include mostly non-imputed data
-    data = data[cols][data.apply(imputationThreshold, axis=1)]
-    print(data)
-
-    X = data.copy()
-
-    n_components = [n for n in np.arange(0, len(cols)+1)]
+# Perform three tests to check the number of significant PCs
+def testPCsignificance(X, n):
+    n_components = [n for n in np.arange(0, len(COLS)+1)]
     pca_scores, fa_scores = compute_scores(X, n_components)
     n_components_pca = n_components[np.argmax(pca_scores)]
     n_components_fa = n_components[np.argmax(fa_scores)]
 
-    X = data.copy()
+    X = X.copy()
     pca = PCA(svd_solver='full', n_components='mle')
     pca.fit(X)
     n_components_pca_mle = pca.n_components_
 
-    print("best n_components by PCA CV = %d" % n_components_pca)
-    print("best n_components by FactorAnalysis CV = %d" % n_components_fa)
-    print("best n_components by PCA MLE = %d" % n_components_pca_mle)
+    print("Best n_components by PCA CV = %d" % n_components_pca)
+    print("Best n_components by FactorAnalysis CV = %d" % n_components_fa)
+    print("Best n_components by PCA MLE = %d" % n_components_pca_mle)
 
-    X = data.copy()
+def runPCA(X, n):
+    X = X.copy()
+    newData = X.copy()
     pca = PCA(svd_solver='full')
     pca.fit(X)
     components = pca.transform(X)
-    print('Loadings')
-    print(pca.explained_variance_)
-    print('Explained variance')
-    print(pca.explained_variance_ratio_)
 
-    pcs = pd.DataFrame(components, columns=['PC1','PC2','PC3','PC4','PC5','PC6','PC7'])
+    print('Loadings: {}'.format(pca.explained_variance_))
+    print('Explained variance: {}'.format(pca.explained_variance_ratio_))
 
-    pcs['Aggregate_metric'] = pcs['PC1'] + pcs['PC2'] + pcs['PC3'] + pcs['PC4']
-    print(pcs['Aggregate_metric'])
+    pcs = np.transpose(components)
+
+    newData['PC1'] = pcs[0]
+    newData['PC2'] = pcs[1]
+    newData['PC3'] = pcs[2]
+    newData['Kinship_index'] = newData['PC1'] + newData['PC2'] + newData['PC3']
+
+    return newData
+
+def mergeNewMetrics(baseData, newData):
+    for metric in ['PC1','PC2','PC3','Kinship_index']:
+        baseData[metric] = newData[metric]
+    return baseData
+
+def main():
+    # Read in data
+    data        = pd.read_csv('ShinySeshatLatLong.csv',index_col=0)
+    dataVanilla = pd.read_csv('shiny-seshat.csv',index_col=0)
+    data['Features_imputed'] = dataVanilla['Features_imputed']
+
+    # Truncate values to the interval [0,1]
+    for col in COLS:
+        data[col] = data[col].apply(truncate)
+
+    # Make four datasets for four imputation treatments
+    for i in range(4):
+        print('######################')
+        print('### data_{}_imputed ###'.format(i))
+        print('######################')
+
+        # Restrict the dataset to only allow i imputed supernatural columns
+        data_i_imputed = restrict(data, i)
+        # Tell us the size
+        print('Size : {} polity-centuries'.format(len(data_i_imputed)))
+        # Print out the results of several PC significance tests
+        testPCsignificance(data_i_imputed, i)
+        # Run the actual PCA
+        data_i_imputed = runPCA(data_i_imputed, i)
+        # Merge the PC data to the base dataset
+        full_data_i = mergeNewMetrics(data, data_i_imputed)
+        # Export to CSV
+        full_data_i.to_csv('seshat-kinship_allow-{}.csv'.format(i))
+        print('')
+
+#    pcs = pd.DataFrame(components, columns=['PC1','PC2','PC3','PC4','PC5','PC6','PC7'])
+
+#    pcs['Aggregate_metric'] = pcs['PC1'] + pcs['PC2'] + pcs['PC3'] + pcs['PC4']
+#    print(pcs['Aggregate_metric'])
 #    sns.histplot(pcs['Aggregate_metric'],bins=20)
 #    plt.show()
 
-    pcs.index = data.index
-    pcs.to_csv('PCA_kinship_metric.csv')
+#    pcs.index = data.index
+#    pcs.to_csv('PCA_kinship_metric.csv')
 
 #    fig = plt.figure()
 #    ax = Axes3D(fig)
@@ -125,7 +151,7 @@ def main():
 #    print(pcs)
 #    ax.scatter(pcs[0],pcs[1],pcs[2])
 #    ax.set_xlabel('PC1')
-#    ax.set_ylabel('PC2')
+#    ax.set_ylabel('PC3')
 #    ax.set_zlabel('PC3')
 #    plt.show()
 
@@ -142,8 +168,7 @@ if __name__ == '__main__':
 #print(pca.explained_variance_ratio_)
 #
 #loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
-#loading_matrix = pd.DataFrame(loadings, columns=['PC1','PC2','PC3'], index=cols)
-#print(loading_matrix)
+#loading_matrix = pd.DataFrame(loadings, columns=['PC1','PC2','PC3'], index=COLS) #print(loading_matrix)
 #
 #pcs = (pd.DataFrame(components,columns=['PC1','PC2','PC3']))
 #
